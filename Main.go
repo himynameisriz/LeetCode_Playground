92e047f8cde66bf264cbd90d4036b38502733b39
Riz Virani <v.rizwanali@gmail.com>
2017-04-05 17:38:50 -0500

    Adding in all caps letters, breaks in leetcode for unkown reason

--- Main.go
+++ Main.go
@@ -8,4 +8,8 @@ import (
 func main() {
 	fb := modules.FizzBuzz{}
 	fmt.Println(fb.String(15))
+
+	// should work, breaks on leetcode
+	capsLetters := modules.AllCapsLetters{}
+	fmt.Println("Detecting all caps", capsLetters.DetectCapitalUse("g"))
 }
